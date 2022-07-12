c811954c16f179f63250c3b1d3fe022eab765509
blitzmartin <lizmartinmusic@gmail.com>
2022-07-12 11:27:11 +0200

    implemented O and X win

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,34 +1,85 @@
-import React, { useState } from 'react';
-import Square from './Square';
+import React, { useState } from "react";
+import Square from "./Square";
 
 const INITIAL = "";
 const X_PLAYER = "X";
-const O_PLAYER = "O"
+const O_PLAYER = "O";
+const winCombination = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
 
 export default function Game() {
-    const [ grid, setGrid ] = useState(Array(9).fill(INITIAL));
-    const [ player, setPlayer ] = useState(false);
+  const [grid, setGrid] = useState(Array(9).fill(INITIAL));
+  const [player, setPlayer] = useState(false);
+  const [win, setWin] = useState(false);
+  const [draw, setDraw] = useState(false);
 
-    const handleClick = (id) => {
-        console.log("clicked")
-        setGrid(
-            grid.map((item, index) => {
-                if (index === id) {
-                    if (player) {
-                        return X_PLAYER;
-                    } else {
-                        return O_PLAYER;
-                    }
-                } else {
-                    return item;
-                }
-            })
-        );
-        setPlayer(!player);
+  const isGameOver = () => {
+    if (!win) {
+      // X wins
+      for (let i = 0; i < 8; i++) {
+        if (
+          grid[winCombination[i][0]] === X_PLAYER &&
+          grid[winCombination[i][1]] === X_PLAYER &&
+          grid[winCombination[i][2]] === X_PLAYER
+        ) {
+          setWin(true);
+          console.log(X_PLAYER);
+        }
+      }
+
+      // O wins
+      for (let i = 0; i < 8; i++) {
+        if (
+          grid[winCombination[i][0]] === O_PLAYER &&
+          grid[winCombination[i][1]] === O_PLAYER &&
+          grid[winCombination[i][2]] === O_PLAYER
+        ) {
+          setWin(true);
+          console.log(O_PLAYER);
+        }
+      }
+
+      // draw game
+      if (!grid.includes(INITIAL)) {
+        //checks if squares are not all empty
+        setDraw(true);
+        setWin(true);
+        console.log("DRAW");
+      }
     }
+  };
+
+  isGameOver();
+
+  const handleClick = (id) => {
+    console.log("clicked");
+    setGrid(
+      grid.map((item, index) => {
+        if (index === id) {
+          if (player) {
+            return X_PLAYER;
+          } else {
+            return O_PLAYER;
+          }
+        } else {
+          return item;
+        }
+      })
+    );
+    setPlayer(!player);
+  };
   return (
     <div>
-       <Square clickedArray={grid} handleClick={handleClick}/>
+      <h1>Play Tic Tac Toe!</h1>
+      <Square clickedArray={grid} handleClick={handleClick} />
     </div>
-  )
+  );
 }
